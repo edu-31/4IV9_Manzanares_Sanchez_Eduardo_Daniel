e04fdae7e1b477a44a9b8ddd828144d500bff6db
edu-31 <emanzanaress2400@alumno.ipn.mx1>
2026-03-03 20:50:00 -0600

    entrada de datos

---
+++ 02EntradaDatos/EntradaTexto.java
@@ -0,0 +1,23 @@
+/*
+Vamos a crear una instancia de un objeto para la entrada y salida de datos 
+*/
+
+//libreria scanner
+
+import java.util.Scanner;
+
+class EntradaTexto{
+    //Metodo principal
+    public static void main(String[] args) {
+        //instanteamos el objeto
+        Scanner .
+        // las variables locales
+        String nombre;
+        int edad;
+        //entrada 
+    System.out.println("Escribe tu nombre: ");
+    nombre = System.console().readLine();
+
+
+    }
+}
