9b96092e70db33756c08821016b12832b8d727b2
edu-31 <emanzanaress2400@alumno.ipn.mx1>
2026-05-15 17:54:25 -0600

    Array list

---
+++ 11ArrayList/src/main/java/com/mycompany/arraylist/App.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arraylist;
+
+/**
+ *
+ * @author k31
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        VistaPersona ventana = new VistaPersona();
+        ventana.Principal();
+    }
+}
