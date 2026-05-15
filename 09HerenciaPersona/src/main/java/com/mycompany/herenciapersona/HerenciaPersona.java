e06e25adac775c961a30c0b1ef4630981717805f
edu-31 <emanzanaress2400@alumno.ipn.mx1>
2026-05-15 17:17:14 -0600

    CRUD Estudiantes

---
+++ 09HerenciaPersona/src/main/java/com/mycompany/herenciapersona/HerenciaPersona.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.herenciapersona;
+
+/**
+ *
+ * @author k31
+ */
+public class HerenciaPersona {
+
+    public static void main(String[] args) {
+        DA0Estudiante objmenu = new DA0Estudiante();
+        objmenu.menu();
+    }
+}
